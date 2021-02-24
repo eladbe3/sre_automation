44381dd0fcf6307554028291e272352bd8313735
eladbe3 <79286016+eladbe3@users.noreply.github.com>
2021-02-24 22:36:01 +0200

    Update AwsScript.py

--- AwsScript.py
+++ AwsScript.py
@@ -1,77 +1,87 @@
 import boto3
 
-vpc_name = params.vpc_name
-app_server_name = params.app_server_name
-open_vpn_name = params.open_vpn
-
+# Create new VPC & Assign a name to our VPC
 ec2 = boto3.resource('ec2')
-
-# create VPC
-vpc = ec2.create_vpc(CidrBlock='172.16.0.0/16')
-
-# assign a name to our VPC
-vpc.create_tags(Tags=[{"Key": "Name", "Value": vpc_name}])
+vpc = ec2.create_vpc(CidrBlock='10.1.0.0/16')
+vpc.create_tags(Tags=[{"Key": "Name", "Value": "BlueVine"}])
 vpc.wait_until_available()
 
-# enable public dns hostname so that we can SSH into it later
+# Enable public dns hostname so that we can SSH into it later
 ec2Client = boto3.client('ec2')
 ec2Client.modify_vpc_attribute( VpcId = vpc.id , EnableDnsSupport = { 'Value': True } )
 ec2Client.modify_vpc_attribute( VpcId = vpc.id , EnableDnsHostnames = { 'Value': True } )
 
-# create an internet gateway and attach it to VPC
+# Create an internet gateway and attach it to VPC
 internetgateway = ec2.create_internet_gateway()
 vpc.attach_internet_gateway(InternetGatewayId=internetgateway.id)
+internetgateway.create_tags(Tags=[{"Key": "Name", "Value": "BlueVine"}])
 
-# create a route table and a public route
-routetable = vpc.create_route_table()
-route = routetable.create_route(DestinationCidrBlock='0.0.0.0/0', GatewayId=internetgateway.id)
+# Creating the VPC also automatically creates a route table so I don't need to create an additional
+for route_table in vpc.route_tables.all():  # There should only be one route table to start with
+        route_table.create_route(DestinationCidrBlock='0.0.0.0/0', GatewayId=internetgateway.id)
+        route_table.create_tags(Tags=[{"Key": "Name", "Value": "BlueVine"}])
 
-# create subnet and associate it with route table
-subnet = ec2.create_subnet(CidrBlock='172.16.1.0/24', VpcId=vpc.id)
-routetable.associate_with_subnet(SubnetId=subnet.id)
+# Create subnet and associate it with route table
+subnet = ec2.create_subnet(CidrBlock='10.1.1.0/24', VpcId=vpc.id)
+subnet.create_tags(Tags=[{"Key": "Name", "Value": "BlueVine"}])
+for route_table in vpc.route_tables.all():
+        route_table.associate_with_subnet(SubnetId=subnet.id)
 
 # Create a security group and allow SSH inbound rule through the VPC
-securitygroup = ec2.create_security_group(GroupName='SSH-ONLY', Description='only allow SSH traffic', VpcId=vpc.id)
+securitygroup = ec2.create_security_group(GroupName='BlueVine-SSH-ONLY', Description='only allow SSH traffic', VpcId=vpc.id)
 securitygroup.authorize_ingress(CidrIp='0.0.0.0/0', IpProtocol='tcp', FromPort=22, ToPort=22)
+securitygroup.create_tags(Tags=[{"Key": "Name", "Value": "BlueVine"}])
 
 # create a file to store the key locally
-outfile = open('ec2-keypair.pem', 'w')
+outfile_app = open('APP-ec2-keypair.pem', 'w')
 
 # call the boto ec2 function to create a key pair
-key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
+key_pair_app = ec2.create_key_pair(KeyName='APP-ec2-keypair')
 
 # capture the key and store it in a file
-KeyPairOut = str(key_pair.key_material)
-outfile.write(KeyPairOut)
+KeyPairOut_app = str(key_pair_app.key_material)
+outfile_app.write(KeyPairOut_app)
 
-# Create a linux instance in the subnet APP SERVER CREATION 
- app_server_instances = ec2.create_instances(
- ImageId='ami-0de53d8956e8dcf80',
- InstanceType='t2.micro',
- MaxCount=1,
- MinCount=1,
- NetworkInterfaces=[{
- 'SubnetId': subnet.id,
- 'DeviceIndex': 0,
- 'AssociatePublicIpAddress': True,
- 'Groups': [securitygroup.group_id]
- }],
- KeyName='ec2-keypair')
+# Create a linux instance in the subnet- APP SERVER CREATION
+app_server_instances = ec2.create_instances(
+        ImageId='ami-0fc970315c2d38f01',
+        InstanceType='t2.micro',
+        MaxCount=1,
+        MinCount=1,
+        NetworkInterfaces=[{
+        'SubnetId': subnet.id,
+        'DeviceIndex': 0,
+        'AssociatePublicIpAddress': True,
+        'Groups': [securitygroup.group_id]
+        }],
+        KeyName='APP-ec2-keypair')
 
-# Create a linux instance in the subnet OPEN_VPN SERVER Creation
- open_vpn_server_instances = ec2.create_instances(
- ImageId='ami-0de53d8956e8dcf80',
- InstanceType='t2.micro',
- MaxCount=1,
- MinCount=1,
- NetworkInterfaces=[{
- 'SubnetId': subnet.id,
- 'DeviceIndex': 0,
- 'AssociatePublicIpAddress': True,
- 'Groups': [securitygroup.group_id]
- }],
- KeyName='ec2-keypair')
+# create a file to store the key locally
+outfile_vpn = open('VPN-ec2-keypair.pem', 'w')
 
-# Change the mode of the file on which we stored our key pair to read-only mode 
-# This is require if you want to SSH into your Linux virtual machines in AWS. 
- chmod 400 ec2-keypair.pem
+# call the boto ec2 function to create a key pair
+key_pair_vpn = ec2.create_key_pair(KeyName='VPN-ec2-keypair')
+
+# capture the key and store it in a file
+KeyPairOut_vpn = str(key_pair_vpn.key_material)
+outfile_vpn.write(KeyPairOut_vpn)
+
+# Create a linux instance in the subnet- OpenVPN SERVER CREATION
+openvpn_server_instances = ec2.create_instances(
+        ImageId='ami-0fc970315c2d38f01',
+        InstanceType='t2.micro',
+        MaxCount=1,
+        MinCount=1,
+        NetworkInterfaces=[{
+        'SubnetId': subnet.id,
+        'DeviceIndex': 0,
+        'AssociatePublicIpAddress': True,
+        'Groups': [securitygroup.group_id]
+        }],
+        KeyName='VPN-ec2-keypair')
+
+
+# Change the mode of the file on which we stored our key pair to read-only mode
+# This is require to SSH into the Linux virtual machines in AWS.
+#chmod 400 APP-ec2-keypair*****************************************************************
+
