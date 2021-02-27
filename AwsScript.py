9185562d0af4edde58e498496318946923937818
eladbe3 <eladbe3@gmail.com>
2021-02-28 01:24:44 +0200

    uoate

--- AwsScript.py
+++ AwsScript.py
@@ -99,7 +99,7 @@ ec2_client = boto3.client('ec2')
 response = ec2_client.describe_instances()
 
 # Creating new List of Instance Ids for all running instances
-instances = response['Reservations']
+instances = response['GroupName']
 print(instances)
 # instance_ids = []
 # for i in instances:
