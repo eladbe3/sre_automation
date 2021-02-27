5a9e80f7dc8e3d081e64e9ed4d2a0fcb465ca2b5
eladbe3 <eladbe3@gmail.com>
2021-02-28 00:18:51 +0200

    uoate

--- AwsScript.py
+++ AwsScript.py
@@ -3,10 +3,10 @@ import os
 
 # Using Environment Variables From User Input
 vpc_name = os.environ['VPC_NAME']
-vpn_srv_name = os.environ['VPN_SRV_NAME']
-app_srv_name = os.environ['APP_SRV_NAME']
+# vpn_srv_name = os.environ['VPN_SRV_NAME']
+# app_srv_name = os.environ['APP_SRV_NAME']
 
-# Create new VPC & Assign a name to our VPC
+# Create new VPC & assign a name to our VPC from user input
 ec2 = boto3.resource('ec2')
 vpc = ec2.create_vpc(CidrBlock='10.1.0.0/16')
 vpc.create_tags(Tags=[{"Key": "Name", "Value": vpc_name}])
@@ -22,12 +22,12 @@ internetgateway = ec2.create_internet_gateway()
 vpc.attach_internet_gateway(InternetGatewayId=internetgateway.id)
 internetgateway.create_tags(Tags=[{"Key": "Name", "Value": vpc_name}])
 
-# Creating the VPC also automatically creates a route table so I don't need to create an additional
+# Creating new VPC also automatically creates a route table so I don't need to create an additional
 for route_table in vpc.route_tables.all():  # There should only be one route table to start with
         route_table.create_route(DestinationCidrBlock='0.0.0.0/0', GatewayId=internetgateway.id)
         route_table.create_tags(Tags=[{"Key": "Name", "Value": vpc_name}])
 
-# Create subnet and associate it with the new route table that automatically created with new VPC
+# Create subnet and associate it with the new route table
 subnet = ec2.create_subnet(CidrBlock='10.1.1.0/24', VpcId=vpc.id)
 subnet.create_tags(Tags=[{"Key": "Name", "Value": vpc_name}])
 for route_table in vpc.route_tables.all():
@@ -60,32 +60,32 @@ app_server_instance = ec2.create_instances(
         'AssociatePublicIpAddress': True,
         'Groups': [securitygroup.group_id]
         }],
-        KeyName='APP-ec2-keypair'
-        TagSpecifications=[{'ResourceType': 'instance',
-                                'Tags': [app_srv_name]}])
+        KeyName='APP-ec2-keypair')
 
+# Create a file to store the key locally
+outfile_vpn = open('VPN-ec2-keypair.pem', 'w')
 
-# # create a file to store the key locally
-# outfile_vpn = open('VPN-ec2-keypair.pem', 'w')
-#
-# # call the boto ec2 function to create a key pair
-# key_pair_vpn = ec2.create_key_pair(KeyName='VPN-ec2-keypair')
-#
-# # capture the key and store it in a file
-# KeyPairOut_vpn = str(key_pair_vpn.key_material)
-# outfile_vpn.write(KeyPairOut_vpn)
-#
-# # Create a linux instance in the subnet- OpenVPN SERVER CREATION
-# openvpn_server_instance = ec2.create_instances(
-#         ImageId='ami-0fc970315c2d38f01',
-#         InstanceType='t2.micro',
-#         MaxCount=1,
-#         MinCount=1,
-#         NetworkInterfaces=[{
-#         'SubnetId': subnet.id,
-#         'DeviceIndex': 0,
-#         'AssociatePublicIpAddress': True,
-#         'Groups': [securitygroup.group_id]
-#         }],
-#         KeyName='VPN-ec2-keypair')
-# openvpn_server_instance.create_tags({"Name": vpn_srv_name})
+# Call the boto ec2 function to create a key pair
+key_pair_vpn = ec2.create_key_pair(KeyName='VPN-ec2-keypair')
+
+# Capture the key and store it in a file
+KeyPairOut_vpn = str(key_pair_vpn.key_material)
+outfile_vpn.write(KeyPairOut_vpn)
+
+# Create a linux instance in the subnet- OpenVPN SERVER CREATION
+openvpn_server_instance = ec2.create_instances(
+        ImageId='ami-0fc970315c2d38f01',
+        InstanceType='t2.micro',
+        MaxCount=1,
+        MinCount=1,
+        NetworkInterfaces=[{
+        'SubnetId': subnet.id,
+        'DeviceIndex': 0,
+        'AssociatePublicIpAddress': True,
+        'Groups': [securitygroup.group_id]
+        }],
+        KeyName='VPN-ec2-keypair')
+
+# ec2_client = boto3.client('ec2')
+# response = ec2_client.describe_instances()
+# ins
